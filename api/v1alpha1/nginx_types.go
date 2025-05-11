bb4d22cc1c0da2c163acb338eb2b8dd960539407
Gyalgin Sherpa <Gyalgin.Sherpa@ibm.com>
2025-05-11 08:17:22 -0700

    Update types.go file with correct data to create CR

--- api/v1alpha1/nginx_types.go
+++ api/v1alpha1/nginx_types.go
@@ -29,13 +29,14 @@ type NginxSpec struct {
 	// Important: Run "make" to regenerate code after modifying this file
 
 	// Foo is an example field of Nginx. Edit nginx_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Replicas *int32 `json:"replicas"`
 }
 
 // NginxStatus defines the observed state of Nginx
 type NginxStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	AvailableReplicas int32 `json:"availableReplicas"`
 }
 
 // +kubebuilder:object:root=true
